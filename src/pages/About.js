f880866e9b68cfa6e6b9185d4e3cd4a999038e11
VascoMiguens <v_miguens@hotmail.com>
2023-02-26 15:12:51 +0000

    - about page with a presentation text and a lottie react gif

---
+++ src/pages/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+import programming from "../programming.json";
+import Lottie from "lottie-react";
+import "./About.css";
+
+export default function About() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <>
+      <div className="container">
+        <h1>About</h1>
+        <div className="about">
+          <div className="presentation">
+            <p>
+              Meet Vasco Miguéns, the full stack web developer and
+              jack-of-all-trades in the web development world. With a passion
+              for sports and computers (yes, it's a weird mix, but it works for
+              him), he traded in sunny beaches and sunsets with friends in his
+              home country for the chance to pursue his passion in chilly
+              England. He's been honing his skills for the past year and
+              recently completed a full stack bootcamp to give structure to his
+              wild ideas and turn them into reality. Now, he's ready to tackle
+              any project thrown his way, make the internet a better place, one
+              website at a time, all while missing the beach and the sunsets
+              with his friends
+            </p>
+          </div>
+          <div style={{ width: "30%" }}>
+            <Lottie options={defaultOptions} animationData={programming} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
