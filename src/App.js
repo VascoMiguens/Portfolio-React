2e622e3930d3dce8c101e987b669675380dacaa7
VascoMiguens <v_miguens@hotmail.com>
2023-03-04 02:52:40 +0000

    - removed hashrouter

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Resume from "./pages/Resume";
 import Footer from "./components/Footer";
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter basename="/">
-      <>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
-          </Routes>
-        </div>
-        <Footer />
-      </>
-    </HashRouter>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
