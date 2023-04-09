c3bd085c3c6f48c5c824143d9c6869b37e37fffb
VascoMiguens <v_miguens@hotmail.com>
2023-04-09 20:54:23 +0100

    - reorganised app.js to land on home page with the layout component surrounding every page - added home page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Layout from "./components/Layout";
 import Navbar from "./components/Navbar";
-import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Resume from "./pages/Resume";
-import Footer from "./components/Footer";
+import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="wrapper">
+    <>
       <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-      </div>
-      <Footer />
-    </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="resume" element={<Resume />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
